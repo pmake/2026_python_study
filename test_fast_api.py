46092654310f3e8b087dc1a571b06feafbc0a6c1
pmake <pmake@users.noreply.github.com>
2026-07-15 05:40:54 +0000

    將.venv*加入gitignore檔案，因應後續如果需要增加多個虛擬環境時避免被誤追蹤。 實作fastAPI的路由參數語法

--- test_fast_api.py
+++ test_fast_api.py
@@ -5,7 +5,19 @@ app = FastAPI()
 
 @app.get("/")
 async def root():
-    return {"message": "Hello!"}
+    return {"message": "Helloaa!"}
+
+
+@app.get('/hello/{name}')
+async def hello(name: str):
+    return {"message": name}
+
+@app.get('/item/')
+async def get_item(limit: int = 3):
+    return {}
+    items = ['book1', 'book2', 'book3']
+    return {'items': items[:limit]}
+
 
 
 # 讓檔案可以直接被 python 指令執行
