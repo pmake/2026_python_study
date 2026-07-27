3a64b9764c247008e5607cfce3419f0f1834f629
pmake <pmake@users.noreply.github.com>
2026-07-27 01:58:24 +0000

    練習supabase"!inner"語法進行join查詢。 將查詢結果轉為pandas dataframe物件然後轉存為csv檔案。

--- 02_data_analysis/nba_analysis.py
+++ 02_data_analysis/nba_analysis.py
@@ -1,17 +1,42 @@
 # 💡 鏈式查詢範例：過濾、排序與限制筆數
 from database import supabase
 import pandas as pd
+from datetime import datetime
+from pathlib import Path
 
+team_full_name = 'Los Angeles Lakers'
 
 response = (
     supabase
     .schema('nba') # 單次查詢時動態指定 Schema的方式
     .table("players")
-    .select("*")  # 選擇特定欄位
-    .limit(10)  # LIMIT 10
+    # 使用supabase !inner語法，join teams table 選擇回傳特定欄位, 例中要求傳回fullname, 留空則不傳回teams table的任何欄位
+    # players和teams在資料庫中必須有設定關聯的外鍵，此例已在supabase設定好了teamid為foreign key，背後會先用外鍵去關聯然後用team_full_name過濾
+    # supabase中的RLS如果有開，也要設定能讀取的policy才能正常從外部查詢
+    .select("*, teams!inner(fullname)")  
+    .eq('teams.fullname', team_full_name)
     .execute()
 )
 
 # 直接轉為 Pandas DataFrame
 df = pd.DataFrame(response.data)
 print(df)
+
+if df.empty:
+    print('查無資料，未產生csv檔。')
+else:
+    output_dir =  Path('02_data_analysis/outputs')
+    output_dir.mkdir(parents=True, exist_ok=True) # 自動建立資料夾(若不存在)
+
+    # 加上時間戳記，避免覆蓋舊檔案
+    timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
+    file_path = output_dir / f'lakers_players_{timestamp}.csv'
+
+    # 4. 儲存為 CSV
+    df.to_csv(
+        file_path,
+        index=False,  # 關鍵設定：不儲存 Pandas 自動產生的數字索引列 (0, 1, 2...)
+        encoding="utf-8-sig",  # 關鍵設定：加上 BOM 頭，確保 Excel 打開中文時不亂碼
+    )
+
+    print(f"✅ 成功將 {len(df)} 筆資料匯出至：{file_path.resolve()}")
